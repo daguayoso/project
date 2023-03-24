e0f9c9407dbb364898936ead0a5f88d2795c75dc
daguayoso <123704835+daguayoso@users.noreply.github.com>
2023-03-24 00:46:45 -0500

    Add files via upload

---
+++ pages/FuelQuoteHistoryTable.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function FuelQuoteHistoryTable({ fuelQuoteHistory }) {
+  return (
+    
+    <table>
+      <thead>
+        <tr>
+          <th>Gallons Requested</th>
+          <th>Delivery Date</th>
+          <th>Delivery Address</th>
+          <th>Suggested Price / Gallon</th>
+          <th>Total Amount Due</th>
+        </tr>
+      </thead>
+      <tbody>
+        {fuelQuoteHistory.map((fuelQuote, index) => (
+          <tr key={index}>
+            <td>{fuelQuote.gallons}</td>
+            <td>{fuelQuote.deliveryDate}</td>
+            <td>{fuelQuote.address}</td>
+            <td>{fuelQuote.suggestedPrice}</td>
+            <td>{fuelQuote.totalAmountDue}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default FuelQuoteHistoryTable;
