072f4069771dec95e4d84d66491c88233d1d54c4
ktran44 <98405222+ktran44@users.noreply.github.com>
2023-04-02 21:55:42 -0500

    Update login.test.js

--- group-project-server/__test__/login.test.js
+++ group-project-server/__test__/login.test.js
@@ -1,17 +1,21 @@
-const supertest = require('supertest')
-const loginService = require('../routes/login');
+const request = require('supertest');
 
-const app = require('express');
+const app = require('../routes/login');
 
-
-
-describe ('login', ()=>{
-
-    describe ('sign in (GET)', ()=>{
-
-        it ("should return status code 200", async () =>{
-        await supertest(app).get('/login/register').expect(200)
-        });
-        
+describe("POST", ()=>{
+  test("Adds a user with username and password", async()=>{
+    const response = await (await request(app).post("/register")).send({
+      username: "username",
+      password: "password"
     })
+    expect(response.statusCode(200))
+  })
+})
+
+
+describe("GET", ()=>{
+  test("Returns a user with statusCode of 200 if username found in database", async()=>{
+    const response = await request(app).get("/signin/joseph2003/jennifer2006")
+    expect(response.statusCode(200))
+  })
 })
